4e60d0e5f88f645deaa4699df26b79f4090c8177
duylinhth7 <hoanglinhdz2k4@gmail.com>
2025-06-30 23:10:31 +0700

    update HOME

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -1,8 +1,29 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
 // [GET] /
-export const home = async (req:Request, res:Response):Promise<void> => {
+export const home = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const products = await sequelize.query(
+        `SELECT product.*, categories.title as categoryTitle, product.price * (1 - product.discount/100) as special_price
+        FROM product 
+        INNER JOIN categories 
+        ON product.categoryId = categories.id
+        WHERE product.deleted = 0
+        `, 
+        {
+      type: QueryTypes.SELECT,
+    });
+    for(const item of products){
+        item["images"] = JSON.parse(item["images"]);
+        item["image"] = item["images"][0];
+    }
     res.render("client/pages/home/index", {
-        title: "Trang chủ"
-    })
-}
+      title: "Trang chủ",
+      productsFeatured: products
+    });
+  } catch (error) {
+    console.log("Lỗi:" , error)
+  }
+};
